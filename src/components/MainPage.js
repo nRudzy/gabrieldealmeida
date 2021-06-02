d758868bd314d472d71d9e307d4b70243a39bd48
gabriel <gabriel.dealmeida69@gmail.com>
2021-06-02 23:19:19 +0200

    adding to top btn

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -11,6 +11,21 @@ const navigation = [
 export default function MainPage() {
     return (
         <div>
+            <div className="w-full bg-none fixed bottom-0 flex justify-end">
+                <AnchorLink href="#homepage" className="p-3 mr-4 mb-4 text-right bg-yellow-300 rounded-full">
+                    <svg height="511pt"
+                         className="h-4 w-4 fill-current text-black animate-pulse"
+                         viewBox="0 0 511 511.9" width="511pt" xmlns="http://www.w3.org/2000/svg">
+                        <g>
+                            <g>
+                                <path d="M466.22,205.787L263.553,3.12c-4.16-4.16-10.923-4.16-15.083,0L45.804,205.787c-21.803,21.803-21.803,57.28,0,79.083
+                                    s57.28,21.803,79.083,0l77.781-77.781v251.584c0,29.397,23.936,53.333,53.333,53.333s53.333-23.936,53.333-53.333V207.088
+                                    l77.781,77.781c21.803,21.803,57.28,21.803,79.083,0C488.001,263.088,488.001,227.589,466.22,205.787z"/>
+                            </g>
+                        </g>
+                    </svg>
+                </AnchorLink>
+            </div>
             <div id="homepage" className="bg-hero-bg bg-no-repeat bg-cover overflow-hidden h-screen">
                 <div className="bg-gray-900 bg-opacity-80 h-full">
                     <div className="max-w-7xl mx-auto">
@@ -35,7 +50,7 @@ export default function MainPage() {
                 </div>
             </div>
             <div id="about" className="bg-gray-800 bg-no-repeat bg-cover overflow-hidden min-h-screen">
-                <div className="max-w-7xl mx-auto">
+                <div className="max-w-3xl mx-auto sm:max-w-7xl sm:mx-auto">
                     <section className="py-24 flex flex-col w-full">
                         <div className="text-center"><span className="bg-yellow-300 px-2">A propos</span></div>
                         <div className="text-center text-4xl text-white capitalize font-bold pt-1">Découvrez en plus !
